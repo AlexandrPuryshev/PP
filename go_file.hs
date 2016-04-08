704dec1c04dbe556f22e351a7318ca4b6d9f7d20
AlexandrPuryshev <Kronos0041@gmail.com>
2016-04-08 07:19:34 +0300

    Result

--- go_file.hs
+++ go_file.hs
@@ -1,14 +1,8 @@
 import System.Environment   
 import System.Directory  
 import System.IO  
-import Data.List 
-
-dispatch :: [(String, [String] -> IO ())]  
-dispatch =  [ ("add", add)  
-            , ("view", view)  
-            , ("remove", remove)
-            , ("view_without_lines", view_without_lines) 
-            ]
+import Data.List
+import Data.Char
 
 view :: [String] -> IO ()  
 view [fileName] = do  
@@ -39,24 +33,79 @@ remove [fileName, numberString] = do
     renameFile tempName fileName
 
 add :: [String] -> IO ()  
-add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")  
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+copyWithisAsciiFilter :: [String] -> IO ()  
+copyWithisAsciiFilter [fileName1, fileName2] = do
+    content <- readFile fileName1
+    writeFile fileName2 (filter isAscii content)
+
+copyWithisHexDigitFilter :: [String] -> IO ()  
+copyWithisHexDigitFilter [fileName1, fileName2] = do
+    content <- readFile fileName1
+    writeFile fileName2 (filter isHexDigit content)
+
+
+input1 :: String -> IO [String]
+input1 arg1 = do
+    putStrLn arg1
+    somethingArg1 <- getLine
+    let string = [somethingArg1]
+    return string
+
+input2 :: String -> String -> IO [String]
+input2 arg1 arg2 = do
+    putStrLn arg1
+    somethingArg1 <- getLine
+    putStrLn arg2
+    somethingArg2 <- getLine
+    let string = [somethingArg1, somethingArg2]
+    return string
+
+--stdin/stdout обрабатываются в текстовом режиме, с автоматической трансляцией \n в OS-specific line delimiter
+
+--По умолчанию для stdout используется построчная буферизация, и это означает, что строки не выводятся до появления символа конца строки ('\n'). 
+--Если вы хотите написать интерактивную программу, то вам может потребоваться отключение буферизации stdout:
 
 main = do
-		let (Just action) = lookup command dispatch
-		putStrLn "Файл обработки текстовых файлов включает следующее:"
-		putStrLn "1: Добавить в файл текст аргументы: (имя файла) (текст)"
-		putStrLn "2: Просмотреть содержимое файла: (имя файла)"
-		putStrLn "3: Просмотреть содержимое файла без нумерации строк: (имя файла)" 
-		putStrLn "4: Удалить строку в файле: (имя файла) (номер строки)"
-		res = readNum
-		args <- getArgs  
-		f res = case res 
-						1 -> let (Just action) = lookup add dispatch
-				        2 -> let (Just action) = lookup view dispatch
-				        3 -> let (Just action) = lookup view_without_lines dispatch
-				        4 -> let (Just action) = lookup remove dispatch - x
-		action args
-		where readNum :: IO Integer
-		readNum = readLn		            
-        --let (Just action) = lookup command dispatch  
-        --action args
+    hSetBuffering stdout NoBuffering
+    putStrLn "Файл обработки текстовых файлов включает следующее:"
+    putStrLn "1: Добавить в файл текст: (имя файла) (текст)"
+    putStrLn "2: Просмотреть содержимое файла: (имя файла)"
+    putStrLn "3: Просмотреть содержимое файла без нумерации строк: (имя файла)" 
+    putStrLn "4: Удалить строку в файле: (имя файла) (номер строки)"
+    putStrLn "5: Копирование содержимого в новый файл с фильтрацией Ascii символов: (имя файла1) (имя файла2)"
+    putStrLn "6: Копирование содержимого в новый файл с фильтрацией на 16-чные символы: (имя файла1) (имя файла2)"
+    res <- readNum
+
+    case res of
+         1 -> do
+            let a = "Введите (имя файла)"
+            let b = "Введите (текст)"
+            parametrs <- input2 a b
+            add parametrs
+         2 -> do
+            let a = "Введите (имя файла)"
+            parametrs <- input1 a
+            view parametrs
+         3 -> do
+            let a = "Введите (имя файла)"
+            parametrs <- input1 a
+            view_without_lines parametrs
+         4 -> do
+            let a = "Введите (имя файла)"
+            let b = "Введите (номер строки)"
+            parametrs <- input2 a b
+            remove parametrs
+         5 -> do
+            let a = "Введите (имя файла1)"
+            let b = "Введите (имя файла2)"
+            parametrs <- input2 a b
+            copyWithisAsciiFilter parametrs
+         6 -> do
+            let a = "Введите (имя файла1)"
+            let b = "Введите (имя файла2)"
+            parametrs <- input2 a b
+            copyWithisHexDigitFilter parametrs
+    putStrLn "Команда выполнена"
+    where readNum :: IO Integer; readNum = readLn		            
